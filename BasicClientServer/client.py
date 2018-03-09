11b985ce5242ab8aaf5ba3d16982047f399fc4f5
Ashley Stallings <aesdev421@gmail.com>
2018-03-08 21:48:14 -0600

    Now works on user input and connections can be close

--- BasicClientServer/client.py
+++ BasicClientServer/client.py
@@ -5,7 +5,25 @@ BUFF = 2048
 
 client = socket.socket()
 client.connect(('127.0.0.1', PORT))
-msg = b"Hello!"
-print("Sending from {0} client".format(msg.decode()))
-client.send(msg)
-print("Recieved {0} from server".format((client.recv(BUFF)).decode()))
+print("Enter messages to be sent to server.  Type quit when you're done.")
+
+while True:
+    msg = input()
+    if (msg == "quit"):
+        client.send(b"FIN")
+        client.recv(BUFF)
+        print("Goodbye")
+        client.close()
+        break
+    msg = msg.encode()
+    print("Sending from {0} client".format(msg.decode()))
+    client.send(msg)
+
+    #recieve server response
+    response = b""
+    while True:
+        data = client.recv(BUFF)
+        response += data
+        if(len(data) < BUFF):
+            break
+    print("Recieved {0} from server".format(response.decode()))
--- BasicClientServer/server.py
+++ BasicClientServer/server.py
@@ -1,7 +1,7 @@
 import socket
 
 PORT = 8080
-BUFF = 1024
+BUFF = 2048
 
 server = socket.socket()
 server.bind(('127.0.0.1', PORT))
@@ -9,15 +9,19 @@ server.bind(('127.0.0.1', PORT))
 server.listen(0)
 print("Server listening on {0}".format(PORT))
 
+(conn, address) = server.accept()
 while True:
-    (conn, address) = server.accept()
     response = b""
     while True:
         data = conn.recv(BUFF)
         response += data
         if(len(data) < BUFF):
             break
-        
+
+    if(response.decode() == 'FIN'):
+        print("Goodbye")
+        conn.close()
+        break
     print("Recieved {0} from client".format(response.decode()))
     print("Echoing {0} from server".format(response.decode()))
     conn.send(response)
