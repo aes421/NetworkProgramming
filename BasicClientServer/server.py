3631a2c3b694ff7a2bd46e6f9682cdd0cfe7ad52
Ashley Stallings <aesdev421@gmail.com>
2018-03-08 22:16:54 -0600

    handle multiple client connections... one after the other

--- BasicClientServer/server.py
+++ BasicClientServer/server.py
@@ -6,22 +6,28 @@ BUFF = 2048
 server = socket.socket()
 server.bind(('127.0.0.1', PORT))
 
-server.listen(0)
+server.listen(2)
 print("Server listening on {0}".format(PORT))
 
-(conn, address) = server.accept()
+#wait for connection
 while True:
-    response = b""
-    while True:
-        data = conn.recv(BUFF)
-        response += data
-        if(len(data) < BUFF):
-            break
+    (conn, address) = server.accept()
 
-    if(response.decode() == 'FIN'):
-        print("Goodbye")
-        conn.close()
-        break
-    print("Recieved {0} from client".format(response.decode()))
-    print("Echoing {0} from server".format(response.decode()))
-    conn.send(response)
+    #client server cycle
+    while True:
+        response = b""
+        
+        #recieve data
+        while True:
+            data = conn.recv(BUFF)
+            response += data
+            if(len(data) < BUFF):
+                break
+
+        if(response.decode() == 'FIN'):
+            print("Goodbye")
+            conn.close()
+            break
+        print("Recieved {0} from client".format(response.decode()))
+        print("Echoing {0} from server".format(response.decode()))
+        conn.send(response)
